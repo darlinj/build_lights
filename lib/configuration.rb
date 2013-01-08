a3449aae87b43daed3c7aa027118bf6051a98b8d
Joe Darling <joe.darling@bt.com>
2013-01-08 13:44:04 +0000

    Better configuration

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -1,5 +1,17 @@
-class Configeration
-  def self.jenkins_url
-    "doo"
+module Configuration
+  extend self
+
+  def parameter *params
+    params.each do |param|
+      attr_accessor param
+      #define_method name do |*values|
+        #value = values.first
+        #value ? self.send("#{name}=", value) : instance_variable_get("@#{name}")
+      #end
+    end
+  end
+
+  def config(&block)
+    instance_eval &block
   end
 end
