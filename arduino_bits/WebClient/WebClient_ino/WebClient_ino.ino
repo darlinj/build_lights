d1d281243af9af6f9d32997d5559ec154ae9e6f0
Joe Darling <joe.darling@bt.com>
2013-02-01 15:33:33 +0000

    working build lights - I think

--- arduino_bits/WebClient/WebClient_ino/WebClient_ino.ino
+++ arduino_bits/WebClient/WebClient_ino/WebClient_ino.ino
@@ -13,55 +13,96 @@ EthernetClient client;
 
 char buffer[5] = "";
 boolean receiving_body = false;
-String status = "";
+String current_status = "";
+String last_status = "other";
 
 // query spacing managment
 const unsigned long request_period = 30000;
 unsigned long last_connected = 0;
 boolean http_connected = false;
 
+//leds
+int green = 2;
+int red = 3;
+
 void setup() {
+
+  pinMode(red, OUTPUT);     
+  pinMode(green, OUTPUT);     
  // Open serial communications and wait for port to open:
   Serial.begin(9600);
    while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-
+  
+  Serial.println("Starting");
+ 
+  Serial.println("Init network");  
   // start the Ethernet connection:
   if (Ethernet.begin(mac) == 0) {
+
     Serial.println("Failed to configure Ethernet using DHCP");
     // no point in carrying on, so do nothing forevermore:
     for(;;)
       ;
   }
+  
   // give the Ethernet shield a second to initialize:
   delay(1000);
+
+  Serial.println("Configured network");
+
 }
 
 void loop()
 {
-  // if there are incoming bytes available 
-  // from the server, read them and print them:
   if (!client.connected() && (millis() - last_connected > request_period)){
     http_connect();
   }
   
   if (client.available()) {
-    char c = client.read();
-    add_to_buffer(c);
-    if(strcmp(buffer,"\r\n\r\n") == 0) {
-      receiving_body = true;
-    }
-    if(receiving_body == true){
-     status += c;
-    }
+    consume_character();
   }
+
+   light_the_lights();
 
   // if the server's disconnected, stop the client:
   if (http_connected && !client.connected()) {
     receiving_body = false;
+    last_status = current_status;
+    current_status = "";
     http_disconnect();
-    status = "";
+  }
+}
+
+void light_the_lights(){
+  if(last_status == "red"){
+    Serial.print("r");
+    digitalWrite(red, HIGH);
+    digitalWrite(green, LOW);
+  }
+  else { 
+    if(last_status == "green"){
+      Serial.print("g");
+      digitalWrite(green, HIGH);
+      digitalWrite(red, LOW);
+    }
+    else {
+      Serial.print("x");
+      digitalWrite(green, LOW);
+      digitalWrite(red, LOW);
+    }
+  }
+}
+
+void consume_character(){
+  char c = client.read();
+  add_to_buffer(c);
+  if(receiving_body == true){
+   current_status += c;
+  }
+  if(strcmp(buffer,"\r\n\r\n") == 0) {
+    receiving_body = true;
   }
 }
 
@@ -74,7 +115,7 @@ void add_to_buffer(char c) {
 
 void http_disconnect(){
     Serial.println("Finished");
-    Serial.println(status);
+    Serial.println(last_status);
     Serial.println();
     Serial.println("disconnecting.");
     client.stop();
